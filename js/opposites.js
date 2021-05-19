3959750d0048fa81ac4984983812235c5f3f2058
Tedi <tgutan@gmail.com>
2021-05-19 22:32:52 +0300

    Add more activities #10 - WIP: added opposite notions code

--- js/opposites.js
+++ js/opposites.js
@@ -1,5 +1,3 @@
-let prevRnd;
-
 // on page load
 jQuery(() => {
   // declared in the html file
@@ -8,9 +6,8 @@ jQuery(() => {
   modalPanel = jQuery('#dialogDiv');
   resultDivElem = jQuery('div.result');
 
-  for (const item of activityItems) {
-    activityObjElemArray.push(jQuery('svg[' + item.name + '-attr]'));
-  }
+  activityObjElemArray.push(jQuery('#itemContainer1Id'));
+  activityObjElemArray.push(jQuery('#itemContainer2Id'));
 
   // show the start icon and let the user manually start the activity
   resultDivElem.fadeIn(300);
@@ -18,19 +15,37 @@ jQuery(() => {
 });
 
 function generateChallengeItems() {
-  do {
-    validItemIndex = Math.floor((Math.random() * activityItems.length));
-  } while (validItemIndex === prevRnd);
-  prevRnd = validItemIndex;
+  let answerOptionValues = getAnswerOptions(); // [true, false, (false)..]
 
-  itemAudioFilePath = activityItems[validItemIndex].audioPath;
-
-  // bind the onclick event function
-  for (const [i, svgElem] of activityObjElemArray.entries()) {
-    svgElem.off('click').click(function () {
-      checkValidAnswer(i === validItemIndex);
-    });
+  // extract the first activityItem
+  let selectedActivityItem1 = activityItems[Math.floor((Math.random() * activityItems.length))];
+  // extract the image to display for this first activity item
+  let imagePath1 = selectedActivityItem1.images[Math.floor((Math.random() * selectedActivityItem1.images.length))];
+  // randomly determine whether this is the correct answer or not
+  let isCorrectAnswer = extractAnswerOption(answerOptionValues);
+  if (isCorrectAnswer) {
+    itemAudioFilePath = selectedActivityItem1.audioPath;
   }
+  let objElem1 = activityObjElemArray[0];
+  objElem1.css('background-image', 'url(' + imagePath1 + ')');
+  objElem1.off('click').click(function () {
+    checkValidAnswer(isCorrectAnswer);
+  });
+
+  // extract the second activityItem from the pairCategory of the first item
+  let selectedActivityItem2 = selectedActivityItem1.pairCategory;
+  // extract the image to display for this second activity item
+  let imagePath2 = selectedActivityItem2.images[Math.floor((Math.random() * selectedActivityItem2.images.length))];
+  // randomly determine whether this is the correct answer or not
+  isCorrectAnswer = extractAnswerOption(answerOptionValues);
+  if (isCorrectAnswer) {
+    itemAudioFilePath = selectedActivityItem2.audioPath;
+  }
+  let objElem2 = activityObjElemArray[1];
+  objElem2.css('background-image', 'url(' + imagePath2 + ')');
+  objElem2.off('click').click(function () {
+    checkValidAnswer(isCorrectAnswer);
+  });
 
   playShowItemAudio();
 }
