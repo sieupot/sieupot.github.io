88269fe029f883d989dd6ba8b69ba6148e2ecf14
Tedi <tgutan@gmail.com>
2021-05-21 16:59:42 +0300

    Add more activities #10 - finished opposite notions

--- js/opposites.js
+++ js/opposites.js
@@ -1,3 +1,5 @@
+let answerOptionValues;
+
 // on page load
 jQuery(() => {
   // declared in the html file
@@ -15,37 +17,30 @@ jQuery(() => {
 });
 
 function generateChallengeItems() {
-  let answerOptionValues = getAnswerOptions(); // [true, false, (false)..]
+  // generate answer options
+  answerOptionValues = getAnswerOptions(); // [true, false, (false)..]
 
   // extract the first activityItem
   let selectedActivityItem1 = activityItems[Math.floor((Math.random() * activityItems.length))];
-  // extract the image to display for this first activity item
-  let imagePath1 = selectedActivityItem1.images[Math.floor((Math.random() * selectedActivityItem1.images.length))];
-  // randomly determine whether this is the correct answer or not
-  let isCorrectAnswer = extractAnswerOption(answerOptionValues);
-  if (isCorrectAnswer) {
-    itemAudioFilePath = selectedActivityItem1.audioPath;
-  }
-  let objElem1 = activityObjElemArray[0];
-  objElem1.css('background-image', 'url(' + imagePath1 + ')');
-  objElem1.off('click').click(function () {
-    checkValidAnswer(isCorrectAnswer);
-  });
+  setupAnswer(activityObjElemArray[0], selectedActivityItem1);
 
   // extract the second activityItem from the pairCategory of the first item
   let selectedActivityItem2 = selectedActivityItem1.pairCategory;
-  // extract the image to display for this second activity item
-  let imagePath2 = selectedActivityItem2.images[Math.floor((Math.random() * selectedActivityItem2.images.length))];
-  // randomly determine whether this is the correct answer or not
-  isCorrectAnswer = extractAnswerOption(answerOptionValues);
-  if (isCorrectAnswer) {
-    itemAudioFilePath = selectedActivityItem2.audioPath;
-  }
-  let objElem2 = activityObjElemArray[1];
-  objElem2.css('background-image', 'url(' + imagePath2 + ')');
-  objElem2.off('click').click(function () {
-    checkValidAnswer(isCorrectAnswer);
-  });
+  setupAnswer(activityObjElemArray[1], selectedActivityItem2);
 
   playShowItemAudio();
+}
+
+const setupAnswer = function (objElem, selectedActivityItem) {
+  // extract the image to display for this first activity item
+  let imagePath1 = selectedActivityItem.images[Math.floor((Math.random() * selectedActivityItem.images.length))];
+  // randomly determine whether this is the correct answer or not
+  let isCorrectAnswer = extractAnswerOption(answerOptionValues);
+  if (isCorrectAnswer) {
+    itemAudioFilePath = selectedActivityItem.audioPath;
+  }
+  objElem.css('background-image', 'url(' + imagePath1 + ')');
+  objElem.off('click').click(function () {
+    checkValidAnswer(isCorrectAnswer);
+  });
 }
