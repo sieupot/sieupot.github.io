5e5935db5fe37caf57a441dd7e69c49f3d7346c3
Tedi <tgutan@gmail.com>
2021-05-31 18:55:21 +0300

    Starting the Spatial Positioning 1 activity

---
+++ js/spatialPositioning.js
@@ -0,0 +1,46 @@
+let answerOptionValues;
+
+// on page load
+jQuery(() => {
+  // declared in the html file
+  initActivityItems();
+
+  modalPanel = jQuery('#dialogDiv');
+  resultDivElem = jQuery('div.result');
+
+  activityObjElemArray.push(jQuery('#itemContainer1Id'));
+  activityObjElemArray.push(jQuery('#itemContainer2Id'));
+
+  // show the start icon and let the user manually start the activity
+  resultDivElem.fadeIn(300);
+  modalPanel.dialog(dialogOptions);
+});
+
+function generateChallengeItems() {
+  // generate answer options
+  answerOptionValues = getAnswerOptions(); // [true, false, (false)..]
+
+  // extract the first activityItem
+  let selectedActivityItem1 = activityItems[Math.floor((Math.random() * activityItems.length))];
+  setupAnswer(activityObjElemArray[0], selectedActivityItem1);
+
+  // extract the second activityItem from the pairCategory of the first item
+  let selectedActivityItem2 = selectedActivityItem1.pairCategory;
+  setupAnswer(activityObjElemArray[1], selectedActivityItem2);
+
+  playShowItemAudio();
+}
+
+const setupAnswer = function (objElem, selectedActivityItem) {
+  // extract the image to display for this first activity item
+  let imagePath1 = selectedActivityItem.images[Math.floor((Math.random() * selectedActivityItem.images.length))];
+  // randomly determine whether this is the correct answer or not
+  let isCorrectAnswer = extractAnswerOption(answerOptionValues);
+  if (isCorrectAnswer) {
+    itemAudioFilePath = selectedActivityItem.audioPath;
+  }
+  objElem.css('background-image', 'url(' + imagePath1 + ')');
+  objElem.off('click').click(function () {
+    checkValidAnswer(isCorrectAnswer);
+  });
+}
