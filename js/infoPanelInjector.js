be1272d9772ce137df499449c8365a9e61d6160a
tgutan <tgutan@gmail.com>
2021-07-07 09:01:04 +0300

    #18 - refactor landing page and select activity pages layout

--- js/infoPanelInjector.js
+++ js/infoPanelInjector.js
@@ -5,13 +5,21 @@ let pgDescriptionMsg = parseDynamicJsString(infoPanel.attr('pgDescriptionMsg'));
 
 let infoPnlContent = `
   <div>
-    <div id="logoId"></div>
-    <div id="logoCircle"></div>
-    <div id="logoImgId"></div>
-    <div id="logoTxtId">
-      <div id="logoTxt1Id">șieu</div>
-      <div id="logoTxt2Id">pot</div>
-    </div>
+    <svg viewBox="0 0 100% 100%" xmlns="http://www.w3.org/2000/svg" height="200">
+      <svg xmlns="http://www.w3.org/2000/svg">
+         <circle cx="55" cy="35" r="160" fill="#F9F4EF" opacity=".23"/>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <circle cx="55" cy="35" r="127" fill="#F9F4EF" opacity=".57"/>
+      </svg>
+
+      <svg xmlns="http://www.w3.org/2000/svg" x="80" y="40">
+        <image xlink:href="images/app/logo_symbol.svg" height="56" width="56"/>
+        <text y="95" style="font: normal normal bold 36px/52px Poppins-600-normal;">
+          <tspan style="fill: #FAA01C;">șieu</tspan><tspan style="fill: #226473;">pot</tspan>
+        </text>
+      </svg>
+    </svg>
   </div>
 
   <div>
@@ -19,10 +27,15 @@ let infoPnlContent = `
     <div id="chooseLevelTextId">${pgDescriptionMsg}</div>
   </div>
 
-  <div id="footerContainerId">
-    <div id="footerCircle"></div>
-    <div id="footerImgId"></div>
-  </div>
+  <svg id="footerContainerId">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <circle cx="122" cy="258" r="211" fill="#F9F4EF" opacity=".57"/>
+    </svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%">
+      <image xlink:href="images/app/children.svg" height="487" width="380" x="-50" y="4"/>
+    </svg>
+  </svg>
 `;
 
 infoPanel.append(infoPnlContent);
