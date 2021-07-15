7a0dc9b3d3a257bff00c89607eb043593841d596
tgutan <tgutan@gmail.com>
2021-07-15 15:03:38 +0300

    #18 - cleanup + refactoring

--- js/injectors/menuUserSettingsInjector.js
+++ js/injectors/menuUserSettingsInjector.js
@@ -12,15 +12,18 @@ let userSettingsContent = `
     <div id="settingsPanelId" class="dropdown-content">
       <div id="settingsPanelTitleId">Setări</div>
 
-      <label for="delayRepeatCommandInputId" style="font: normal normal bold 22px/37px Poppins-300-normal;">Interval repetare comandă: </label>
+      <label for="delayRepeatCommandInputId">Interval repetare comandă: </label>
       <output for="delayRepeatCommandInputId" id="delayOutputId">15</output>
       <output for="delayRepeatCommandInputId" style="font: normal normal bold 22px/37px Poppins-300-italic;"> secunde</output>
       <input id="delayRepeatCommandInputId" type="range" min="5" max="35" step="5" class="slider-round"
              oninput="document.querySelector('#delayOutputId').value = value; setSessionProperty('commandRepeatIntervalSeconds', value);"/>
 
-      <label for="displayActivityTimerInputId" style="font: normal normal bold 22px/37px Poppins-300-normal;">Afișare durată activitate: </label>
-      <input id="displayActivityTimerInputId" type="checkbox"
-             onchange="setSessionProperty('displayActivityTimer', document.querySelector('#displayActivityTimerInputId').checked);"/>
+      <label class="checkbox-container">
+        Afișare durată activitate:
+        <input id="displayActivityTimerInputId" type="checkbox"
+               onchange="setSessionProperty('displayActivityTimer', document.querySelector('#displayActivityTimerInputId').checked);"/>
+        <span class="checkmark"></span>
+      </label>
     </div>
   </div>
 `;
